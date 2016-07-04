bb7b4661eea28393ff6c9370b1b8f710c18286a7
Harpo Jaeger <harpo.jaeger@gmail.com>
2016-07-04 15:53:33 +0200

    delete.js initial commit

--- beauty.js
+++ beauty.js
@@ -15,37 +15,6 @@ function prepareSearch(){
 
 
 
-/** Function to delete a particular event **/
-function deleteThisEvent(calendar_id, event_id) {
-    console.log("Attempting to delete event " + event_id + " cal ID " + calendar_id);
-    var delete_request = gapi.client.calendar.events.delete({
-        'calendarId': calendar_id,
-        'eventId': event_id
-    });
-    delete_request.execute(function(resp) {
-
-        var code = resp.code;
-        if (resp.code) {
-            console.log("Error " + resp.code + ": " + resp.message);
-            console.log(resp);
-        } else {
-            console.log("Deleted " + event_id + "successfully.")
-            var event_div = document.getElementById("event-" + event_id);
-            event_div.parentNode.removeChild(event_div);
-        }
-    });
-}
-
-function buildDeleteLink(div, calendar_id, event_id) {
-    a = document.createElement("a");
-    function_call = "javascript:deleteThisEvent('" + calendar_id + "','" + event_id + "');"
-    a.setAttribute("href", function_call);
-    t = document.createTextNode("(delete)");
-    a.appendChild(t);
-    div.appendChild(a);
-    
-}
-
 function buildInstancesLink(div, calendar_id, event_id, recurring_id) {
 a = document.createElement("a");
 function_call = "javascript:listInstances('" + calendar_id + "','" + recurring_id + "');"
