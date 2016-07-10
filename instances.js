be9a1bf856c514db83c1d9264e1b9664a785f274
Harpo Jaeger <harpo.jaeger@gmail.com>
2016-07-10 13:04:23 +0200

    Output the date of each individual instance in short format

--- instances.js
+++ instances.js
@@ -1,6 +1,6 @@
 function Instance(event){
     this.id = event.id
-    debug("Created new instance with ID " + this.id)
+    /** debug("Created new instance with ID " + this.id) **/
     this.recurringEventId = event.recurringEventId;
     this.summary = event.summary;
     this.start = event.start;
@@ -9,7 +9,14 @@ function Instance(event){
     var when = event.start.dateTime;
     if (!when) {
         when = event.start.date;
+        parsedDate = Date.parse(when);
+        when = parsedDate.toString('M/d/yyyy');
+    } else {
+        parsedDate = Date.parse(when);
+        when = parsedDate.toString('M/d/yyyy HH:mm tt');
+    
     }
+    
 
     var instance_list_item = document.createElement("li");
     var c = document.createTextNode(event.summary + ' (' + when + ') ');
