4fa4557078c2aab2fbeb9e28c5a3d802b35647a4
Harpo Jaeger <harpo.jaeger@gmail.com>
2016-07-23 13:29:55 +0200

    Prevent calendar selector from displaying before calendar list is loaded.

--- cal_list.js
+++ cal_list.js
@@ -1,75 +1,76 @@
-function list_calendars(){
-	debug("Listing calendars");
-	var request = gapi.client.calendar.calendarList.list({
-	"minAccessRole" : "writer"
-	});
-	
-	request.execute(function(resp){
-		var calendars = resp.items;
-		calendar_select = $("#calendar-select");
-		for (i = 0; i < calendars.length; i++) {
-    		var calendar = calendars[i];
-	    	new Calendar(calendar);
-    	}
-    	debug("Retrieved " + calendars.length + " calendars.");
-	});
+function list_calendars() {
+    debug("Listing calendars");
+    var request = gapi.client.calendar.calendarList.list({
+        "minAccessRole": "writer"
+    });
+
+    request.execute(function(resp) {
+        var calendars = resp.items;
+        calendar_select = $("#calendar-select");
+        for (i = 0; i < calendars.length; i++) {
+            var calendar = calendars[i];
+            new Calendar(calendar);
+        }
+        debug("Retrieved " + calendars.length + " calendars.");
+        $('#calendar-select').selectmenu({
+            renderItem: coloredItem
+        });
+        $('#submit').button();
+        $('#search_content').show();
+        $("#calendar-div").show();
+    });
 
 
-	$.widget( "ui.selectmenu", $.ui.selectmenu, {
-		options: {
-			renderItem: null,
-			renderMenu: null
-		},
+    $.widget("ui.selectmenu", $.ui.selectmenu, {
+        options: {
+            renderItem: null,
+            renderMenu: null
+        },
 
-		_renderItem: function( ul, item ) {
-			if ( $.isFunction( this.options.renderItem ) )
-				return this.options.renderItem( ul, item );
-			else
-				return this._super( ul, item );
-			},
+        _renderItem: function(ul, item) {
+            if ($.isFunction(this.options.renderItem))
+                return this.options.renderItem(ul, item);
+            else
+                return this._super(ul, item);
+        },
 
-			_renderMenu: function( ul, items ) {
+        _renderMenu: function(ul, items) {
 
-			if ( $.isFunction( this.options.renderMenu ) ) {
+            if ($.isFunction(this.options.renderMenu)) {
 
-				this.options.renderMenu( ul, items );
+                this.options.renderMenu(ul, items);
 
-			}
+            }
 
-		this._super( ul, items );
-		},
+            this._super(ul, items);
+        },
 
-	});
+    });
 
-    coloredItem = function( ul, item ) {
-		var li = $('<li>');
-		var div = $('<div>');
-		$(div).css({
-			'background-color' : item.element.data('backgroundColor'),
-			'display' : 'inline'
-		});
-		$(div).html('&nbsp;');
-		var a = $( '<a>' ).html( item.label )
-		$(li).append(div).append( a );
+    coloredItem = function(ul, item) {
+        var li = $('<li>');
+        var div = $('<div>');
+        $(div).css({
+            'background-color': item.element.data('backgroundColor'),
+            'display': 'inline'
+        });
+        $(div).html('&nbsp;');
+        var a = $('<a>').html(item.label)
+        $(li).append(div).append(a);
 
-		return $(li).appendTo( ul);
+        return $(li).appendTo(ul);
     };
 
-	$( '#calendar-select' ).selectmenu({
-		renderItem: coloredItem
-	});
-	$('#submit').button();
-	$('#search_content').show();
-	$("#calendar-div").show();
+
 }
 
-function Calendar(c){
-	var o = document.createElement("option");
-	$(o).text(c.summary);
-	$(o).attr("value",c.id);
-	$(calendar_select).append(o);
-	$(o).data({
-		'backgroundColor' : c.backgroundColor,
-		'foregroundColor' : c.foregroundColor
-	});
+function Calendar(c) {
+    var o = document.createElement("option");
+    $(o).text(c.summary);
+    $(o).attr("value", c.id);
+    $(calendar_select).append(o);
+    $(o).data({
+        'backgroundColor': c.backgroundColor,
+        'foregroundColor': c.foregroundColor
+    });
 }
