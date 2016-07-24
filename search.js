936b4819d11cdf3e6cedd02b434f4815f50923a9
Harpo Jaeger <harpo.jaeger@gmail.com>
2016-07-25 00:34:48 +0200

    Explanation of dialog button icons setting

--- search.js
+++ search.js
@@ -25,6 +25,7 @@ function prepareSearch() {
       },
       buttons: [{
         text: "OK",
+        /** The icons setting is broken in jQuery 1.12.0, but I'm leaving this here in case someone (or me!) patches it. Reverting to 1.11.4 broke the selectmenu.**/
         icons: {
           primary: "ui-icon-check",
           secondary: "ui-icon-circle-check"
