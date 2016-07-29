e0f2d372608c22eda3b9b3bf80e3d476d161ab9a
Harpo Jaeger <harpo.jaeger@gmail.com>
2016-07-28 20:52:25 -0400

    Remove extraneous timezone code

--- timezone.js
+++ timezone.js
@@ -1,15 +1,5 @@
 function calculateTimezone() {
-  var d = new Date()
-  /** var offset = d.getTimezoneOffset();
-  if (offset == 0) {
-
-    return 'Z'
-  } else if (offset > 0) {
-    return offset / -60
-  } else {
-    return '+' + offset / -60
-
-  }**/
+  var d = new Date();
   offset = d.getUTCOffset();
   return offset.slice(0,3)+":"+offset.slice(3);
 }
