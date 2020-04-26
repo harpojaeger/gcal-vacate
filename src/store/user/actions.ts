6b0f16392f6b1374db89e890e7c2954a437a2905
Harpo Jaeger <harpo.jaeger@gmail.com>
2020-04-26 11:08:38 -0400

    start switching to the redux-toolkit pattern for store management

--- src/store/user/actions.ts
+++ src/store/user/actions.ts
@@ -1,15 +1,12 @@
-import { SET_SIGNED_IN, SET_CALENDARS, UserActionType } from './types'
+import { createAction } from '@reduxjs/toolkit';
+import { SET_SIGNED_IN, SET_CALENDARS, UserActionType, FETCH_CALENDAR_LIST } from './types'
 
-export function setSignedIn(isSignedIn: boolean): UserActionType {
-    return {
-        type: SET_SIGNED_IN,
-        payload: isSignedIn
-    }
-}
+export const setSignedIn = createAction<boolean>(SET_SIGNED_IN);
 
-export function setCalendars(calendars: gapi.client.calendar.CalendarListEntry[]): UserActionType {
-    return {
-        type: SET_CALENDARS,
-        payload: calendars
-    }
-}
+export const setCalendars = createAction<gapi.client.calendar.CalendarListEntry[]>(SET_CALENDARS);
+
+// export function fetchCalendarList(): Promise<UserActionType> {
+//     return (dispatch: function) => {
+//         return Promise.resolve([]).then(calendars => dispatch(setCalendars(calendars)));
+//     }
+// }
