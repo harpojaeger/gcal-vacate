7a2b6a055f10ef58d5fc4f2ceec9d42015a75af3
Harpo Jaeger <harpo.jaeger@gmail.com>
2020-04-26 14:54:40 -0400

    remove CRA link

--- src/App.tsx
+++ src/App.tsx
@@ -60,14 +60,6 @@ class App extends React.Component<appProps, {}> {
               <CalendarList calendars={this.props.calendars} />
             }
           </div>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
