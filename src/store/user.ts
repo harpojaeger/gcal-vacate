540da553496c5e7b9a4f0158a8dedcaf8bde3862
Harpo Jaeger <harpo.jaeger@gmail.com>
2020-05-16 18:41:08 -0400

    remove the deprecated calendar list from the user state slice

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,5 +1,5 @@
 import { ActionReducerMapBuilder, createAsyncThunk } from '@reduxjs/toolkit';
-import { createAction, createReducer } from '@reduxjs/toolkit'
+import { createReducer } from '@reduxjs/toolkit';
 import { thunkApiExtras } from './root';
 import { signInListener } from '../client/gapi';
 
@@ -11,43 +11,57 @@ export enum UserSignInStatus {
     SIGNED_OUT = 2,
 }
 export interface UserState {
-    signInStatus: UserSignInStatus,
-    calendars: gapi.client.calendar.CalendarListEntry[]
+    signInStatus: UserSignInStatus;
 }
 
 export const initialState: UserState = {
     signInStatus: UserSignInStatus.UNKNOWN,
-    calendars: []
-}
+};
 
-export const setCalendars = createAction<gapi.client.calendar.CalendarListEntry[]>("SET_CALENDARS");
-
-export const requestSignIn = createAsyncThunk<void, void, { extra: thunkApiExtras }>('user/requestSignIn', async (unused, { extra: { rpcClient } }) => {
+export const requestSignIn = createAsyncThunk<
+    void,
+    void,
+    { extra: thunkApiExtras }
+>('user/requestSignIn', async (unused, { extra: { rpcClient } }) => {
     rpcClient.signIn();
 });
 
-export const requestSignOut = createAsyncThunk<void, void, { extra: thunkApiExtras }>('user/requestSignOut', async (unused, { extra: { rpcClient } }) => {
+export const requestSignOut = createAsyncThunk<
+    void,
+    void,
+    { extra: thunkApiExtras }
+>('user/requestSignOut', async (unused, { extra: { rpcClient } }) => {
     rpcClient.signOut();
 });
 
-export const updateSignInStatus = createAsyncThunk<boolean, void, { extra: thunkApiExtras }>('user/updateSignInStatus', async (unused, { extra: { rpcClient } }) => {
+export const updateSignInStatus = createAsyncThunk<
+    boolean,
+    void,
+    { extra: thunkApiExtras }
+>('user/updateSignInStatus', async (unused, { extra: { rpcClient } }) => {
     return rpcClient.getIsSignedIn();
 });
 
-export const setSignInListener = createAsyncThunk<void, signInListener, { extra: thunkApiExtras }>('user/setSignInListener', async (listener, { extra: { rpcClient } }) => {
+export const setSignInListener = createAsyncThunk<
+    void,
+    signInListener,
+    { extra: thunkApiExtras }
+>('user/setSignInListener', async (listener, { extra: { rpcClient } }) => {
     rpcClient.setSignInListener(listener);
 });
 
-export const userReducer = createReducer(initialState, (builder: ActionReducerMapBuilder<UserState>) => {
-    builder.addCase(setCalendars, (state, { payload: calendars }) => ({ ...state, calendars }));
-
-    builder.addCase(updateSignInStatus.fulfilled, (state, { payload: isSignedIn }) => {
-        if (isSignedIn) {
-            state.signInStatus = UserSignInStatus.SIGNED_IN;
-        } else {
-            state.signInStatus = UserSignInStatus.SIGNED_OUT;
-        }
-        // ...state, isSignedIn
-    });
-
-});
+export const userReducer = createReducer(
+    initialState,
+    (builder: ActionReducerMapBuilder<UserState>) => {
+        builder.addCase(
+            updateSignInStatus.fulfilled,
+            (state, { payload: isSignedIn }) => {
+                if (isSignedIn) {
+                    state.signInStatus = UserSignInStatus.SIGNED_IN;
+                } else {
+                    state.signInStatus = UserSignInStatus.SIGNED_OUT;
+                }
+            }
+        );
+    }
+);
